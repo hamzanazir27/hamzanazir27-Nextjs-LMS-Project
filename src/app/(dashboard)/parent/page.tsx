cdf955797ec5c725d1053c0be43da724facdc3fa
hamzanazir27 <hamzaramay51@gmail.com>
2025-10-17 21:12:33 +0500

    create teacherPage UI

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,5 +1,23 @@
+import Announcement from "@/components/Announcement";
+import BigCallender from "@/components/BigCallender";
+import EventCallander from "@/components/EventCallander";
+
 const ParentPage = () => {
-  return <div className="">Parent Page</div>;
+  return (
+    <div className="p-4 h-full flex gap-4 flex-col xl:flex-row">
+      {/* left */}
+      <div className="w-full xl:w-2/3 ">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold"> Schdule (Hamza Nazir)</h1>
+          <BigCallender />
+        </div>
+      </div>
+      {/* Right */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcement />
+      </div>
+    </div>
+  );
 };
 
 export default ParentPage;
